86aab9bf6f72123670f32ea03a0ed65672e202f9
Harry Brundage <harry.brundage@gmail.com>
2018-04-13 14:08:01 -0400

    Make karma exit instead of watch on CI

--- karma.conf.js
+++ karma.conf.js
@@ -52,7 +52,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: !!process.env['CI'],
 
     // Concurrency level
     // how many browser should be started simultaneous
